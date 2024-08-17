98e3194c7cc1fc8e527221ed4a5464396dda5a19
dutenrapha <dutenrapha@hotmail.com>
2024-08-16 23:58:36 -0300

    ETL: ingestion thread version

--- src/ingestion_function.py
+++ src/ingestion_function.py
@@ -3,15 +3,14 @@ import requests
 from bs4 import BeautifulSoup
 import logging
 import os
+from concurrent.futures import ThreadPoolExecutor
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
 
-# Configuração do cliente S3 e logger
 s3_client = boto3.client('s3')
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
 
-# Nome do bucket S3 e URL do GDELT definidos via variáveis de ambiente
 S3_BUCKET_NAME = os.environ['S3_BUCKET_NAME']
 GDELT_URL = 'http://data.gdeltproject.org/events/index.html'
 
@@ -25,20 +24,24 @@ def lambda_handler(event, context):
     zip_links = [link['href'] for link in soup.find_all('a', href=True) if link['href'].endswith('.zip')]
     logger.info(f"Found {len(zip_links)} files to check.")
 
-    new_files = 0
-    for zip_file in zip_links:
-        if not file_exists_in_s3(zip_file):
-            zip_url = f'http://data.gdeltproject.org/events/{zip_file}'
-            if download_and_upload_to_s3(zip_url, zip_file):
-                new_files += 1
-        else:
-            logger.info(f"File {zip_file} already exists in S3, skipping download.")
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        futures = [executor.submit(process_file, zip_file) for zip_file in zip_links]
+        results = [future.result() for future in futures]
 
+    new_files = sum(results)
     logger.info(f"Successfully uploaded {new_files} new files to S3")
     return {
         'statusCode': 200,
         'body': f'Successfully uploaded {new_files} new files to S3'
     }
+
+def process_file(zip_file):
+    if not file_exists_in_s3(zip_file):
+        zip_url = f'http://data.gdeltproject.org/events/{zip_file}'
+        return download_and_upload_to_s3(zip_url, zip_file)
+    else:
+        logger.info(f"File {zip_file} already exists in S3, skipping download.")
+        return 0
 
 def file_exists_in_s3(file_name):
     """Check if the file already exists in the S3 bucket"""
@@ -59,10 +62,10 @@ def download_and_upload_to_s3(zip_url, zip_file):
                 s3_key = f'bronze/gdelt_data/{zip_file}'
                 s3_client.upload_fileobj(response.raw, S3_BUCKET_NAME, s3_key)
                 logger.info(f"Successfully uploaded {zip_file} to S3")
-                return True
+                return 1
             else:
                 logger.error(f"Failed to download {zip_file} from {zip_url}: {response.status_code}")
-                return False
+                return 0
     except Exception as e:
         logger.error(f"Error downloading or uploading file {zip_file}: {e}")
-        return False
+        return 0
